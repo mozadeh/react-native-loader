d0d93bdd013544cf7f59f84e039a87a4cf8d6a38
Mo Ghasemzadeh <mo.zadeh@berkeley.edu>
2020-06-29 11:57:55 -0400

    Update Pulse.js

--- src/Pulse.js
+++ src/Pulse.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Surface } from '@react-native-community/art';
-
+import { Animated } from 'react-native';
 import Circle from './animated/Circle';
 
 export default class Pulse extends Component {
