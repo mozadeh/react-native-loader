ea51f4705e6a8a5241664d1504480ba2f2c2e9d3
Mo Ghasemzadeh <mo.zadeh@berkeley.edu>
2020-06-29 11:57:23 -0400

    Update Bars.js

--- src/Bars.js
+++ src/Bars.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Animated } from 'react-native';
 import { Surface } from '@react-native-community/art';
 
 import Bar from './animated/Bar';
